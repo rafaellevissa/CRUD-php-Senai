d6a5846c3adb495e278a57c76bf99114d03c317e
rafaellevissa <rafaellevissa@gmail.com>
2020-05-28 14:18:48 -0300

    formulario de cadastro

--- src/form_cad_cliente.php
+++ src/form_cad_cliente.php
@@ -1,15 +1,26 @@
 <?php
-echo("conectando no servidor...");
-if(!@($conexao=pg_connect("host = localhost dbname = senai port=5432 user=postgres password=123"))){
-    echo("Não conectou ao banco");
-}else{
-    echo("Conexão ok!");
-}
-
-$sql = "select * from usuarios;";
-$res = pg_query(conexao, $sql);
-while($row=pg_fetch_array($res)){
-    $value= $row["nome"];
-    $options.="<OPTION VALUE=\"$value\">".$value;
-}
+$options="teste";
 ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Rafael Levi">        
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/geral.css">                
+        <title>SENAI</title>
+    </head>
+    <body>
+        <form action="gravar_dados_tabela_usuario.php" method="POST">
+            <div class="form-group">
+                <label for="nome1">Nome: </label>
+                <input type="text" class="form-control" id="nome1" placeholder="Nome completo">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">E-mail: </label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="exemplo@email.com">
+            </div>
+            <button type="submit" class="btn btn-primary">Confirmar</button>
+        </form>                    
+    </body>
+</html>
